ee8a77867d6fd6ce1bf106105d493560c03c5985
Sayli010 <saylipisal6@gmail.com>
2020-01-21 11:57:49 +0530

    postgress with express

--- index.js
+++ index.js
@@ -1,46 +1,70 @@
-// const { Pool , Client } = require('pg');
-// const connectionString = 'postgressql://postgres:root@localhost:5432/test';
-
-// const client = new Client({
-//     connectionString:connectionString
-// })
-
-// client.connect();
-
-
-
 const { Pool, Client } = require('pg')
-const connectionString = 'postgresql://postgres:root@localhost:5432/template1'
-// const pool = new Pool({
-//   connectionString: connectionString,
-// })
-// pool.query('SELECT NOW()', (err, res) => {
-//   console.log(err, res)
-//   pool.end()
-// })
+const express = require('express');
+const path = require('path')
+const bodyParser = require('body-parser')
+
+
+const app = express();
+const publicDirectoryPath = path.join(__dirname, '/views')
+console.log(publicDirectoryPath)
+app.set('view engine', 'html')
+
+app.use(express.static(publicDirectoryPath))  
+app.use(bodyParser.urlencoded({ extended: false })) 
+app.use(bodyParser.json())
+
+
 const client = new Client({
-  connectionString: connectionString,
+  user: 'postgres',
+  host: 'localhost',
+  database: 'Test',
+  password: 'root',
+  port: 5432,
 })
-client.connect(()=>{
-    console.log("connected")
+client.connect()
+app.get('',(req ,res)=>{
+  res.render('index');
+  res.send("HELLO"+req.body);
 })
-// client.query(`CREATE TABLE Persons(
-//     PersonID int,
-//     LastName varchar(255),
-//     FirstName varchar(255))`,(err, res) => {
-//     console.log(err, res)
-//     client.end()
-// })
 
-// client.query(`INSERT INTO Persons (personid,lastname,firstname)VALUES (
-//     2,
-//     'PISAL',
-//     'SAYLI')`,(err, res) => {
-//     console.log(err, res)
-//     client.end()
-// })
-
-client.query(`SELECT * FROM PERSONS`,(err, res) => {
-    console.log(err, res)
-    client.end()
+app.get('/create',(req ,res)=>{
+  client.query(`CREATE TABLE test(
+        AgentId int,
+        UserName varchar(255),
+        Password varchar(255))`,(err, res) => {
+        console.log(err, res)
+        client.end()
+    })
+    res.send("Create")
 })
+
+app.post('/insert',(req ,res)=>{
+    const user = req.body;
+    client.query(`insert into test (agentid, username , password ) values (${user.agentid} ,'${user.username}', '${user.password}')`,(err, res) => {
+        console.log(err, res)
+        client.end()
+    })
+    res.send("Insert")
+})
+
+app.post('/update',(req ,res)=>{
+  const user = req.body;
+  client.query(`UPDATE test SET username ='${user.username}';`,(err, res) => {
+      console.log(err, res)
+      client.end()
+  })
+  res.send("Update")
+})
+
+app.delete('/delete',(req ,res)=>{
+  const user = req.body;
+  client.query(`DELETE FROM test WHERE password = '${user.password}' ;`,(err, res) => {
+      console.log(err, res)
+      client.end()
+  })
+  res.send("delete")
+})
+
+app.listen(3000,()=>{
+  console.log("server start on 3000")
+});
